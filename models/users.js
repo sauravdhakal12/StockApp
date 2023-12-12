4dcd5353d4db8eeacabdc0874072715f16e103ab
sauravdhakal12 <sauravdhakal121@gmail.com>
2023-12-12 18:17:08 +0545

    Create Schema for User (#2)

---
+++ models/users.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+// User will have a display name, email and password
+const userSchema = new mongoose.Schema({
+
+  displayName: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 16,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+});
+
+// Remove id, version and password from returned object
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.password;
+  }
+})
+
+module.exports = mongoose.model('Users', userSchema);
