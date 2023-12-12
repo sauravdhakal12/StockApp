655c3bbb664769284d95b3033fe95507ec5ba182
sauravdhakal12 <sauravdhakal121@gmail.com>
2023-12-12 18:12:44 +0545

    Connected to DB (#1)

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
+const config = require("./utils/config");
+const mongoose = require("mongoose");
 
 // Import router objects
 const stockRouter = require("./controllers/stock");
 const authRouter = require("./controllers/authenticate");
+
+// Connect to DB
+mongoose.connect(config.MONGODB_URI)
+  .then((res) => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // app.use for routes, works as middleware
 app.use("/", stockRouter);  // Home Page
