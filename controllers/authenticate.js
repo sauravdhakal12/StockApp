72fc6159c5f89e95ac1eca282f72a5d104823795
sauravdhakal12 <sauravdhakal121@gmail.com>
2023-12-13 20:58:23 +0545

    Login-in implemented (#4)

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -1,17 +1,58 @@
+const jwt = require('jsonwebtoken')
 const authRouter = require("express").Router();
 const userSchema = require("../models/users");
 const bcrypt = require("bcrypt");
 const logger = require("../utils/logger");
-const userExists = require("./helper/authenticate")
 
-// TODO: add package for rendering files.
-// TODO: signup routes handles post method 
 
-authRouter.get("/login", (req, res) => {
-  return res.send("Login");
+// Check if user with given email exists
+const userExists = async (email) => {
+  try {
+    const res = await userSchema.findOne({ "email": email })
+    return res;
+  }
+  catch (exception) {
+    logger.error(exception);
+    return true;
+  }
+}
+
+
+/**************** 
+  LOGIN
+*****************/
+authRouter.get("/login", async (req, res) => {
+  const body = req.body;
+
+  // Make sure no fields are empty
+  if (!body.displayName || !body.email || !body.password) {
+    return res.status(400).json({ "error": "All fields are required" });
+  }
+
+  // Check to see if user already exists
+  const user = await userExists(body.email);
+  const passswordCorrect = user === null
+    ? false
+    : await bcrypt.compare(body.password, user.password);
+
+  if (!(user && user !== true && passswordCorrect)) {
+    return res.status(401).json({ "error": "Invalid email or password" });
+  }
+
+  // Create and sign token
+  const userJWT = {
+    email: user.email,
+    id: user.id,
+  }
+  const token = jwt.sign(userJWT, process.env.SECRET);
+
+  return res.status(200).send({ token, displayName: user.displayName });
 });
 
 
+/**************** 
+  SIGNUP
+*****************/
 authRouter.post("/signup", async (req, res) => {
   const body = req.body;
 
